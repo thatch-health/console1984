1a618ed2305138dea9bad66930b4001ba5e8e655
Olivier Bellone <olivier@thatch.ai>
2023-06-16 10:31:23 -0700

    wip

--- app/models/console1984/base.rb
+++ app/models/console1984/base.rb
@@ -1,6 +1,14 @@
 module Console1984
   class Base < Console1984.config.base_record_class.constantize
     self.abstract_class = true
+
+    # connecting_to(role: ActiveRecord.writing_role, prevent_writes: false)
+
+    def self.connection
+      c = super
+      p(c)
+      c
+    end
   end
 end
 
